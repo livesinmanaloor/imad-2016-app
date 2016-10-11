aed83a0b5623e202d360b5e40b4208445b309b86
livesinmanaloor <livesinmanaloor@gmail.com>
2016-10-11 20:48:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One | Sunil',
+    heading: 'Article One',
+    date: '11 October 2016',
+    content: `<p>
+            This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One.
+            </p>
+            <p>
+            This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One.
+            </p>
+            <p>
+            This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One. This is the content of Article-One.
+            </p>`
+};
+
+function createTemplate(data)
+{
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>${title}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <div>
+                <h4><font face="Verdana" color="blue">${heading}</font></h4>
+            </div>
+            <div>${date}</div>
+            <div>
+                ${content}
+            </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
@@ -28,7 +75,6 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
