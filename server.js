060e2edff8ff972d427d8c5746703b4a0c9ae884
livesinmanaloor <livesinmanaloor@gmail.com>
2016-10-13 15:04:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ var articles = {
         heading: 'Article One',
         date: '9 October 2016',
         content: `<p>
-            This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. 
+            This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article.
             </p>
             <p>
-            This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. 
+            This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article.
             </p><p>
-            This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. 
+            This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article. This is the content of the first article.
             </p>`
     },
     'article-two': {
@@ -73,7 +73,22 @@ function createTemplate(data) {
 }
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name', function (req, res) {
+    // Get the name from the request
+    var name = req.query.name;
+    names.push(name);
+    // JSON - JavaScript Object Notation
+    res.send(JSON.stringify(names));
 });
 
 app.get('/:articleName', function (req, res) {
@@ -82,14 +97,18 @@ app.get('/:articleName', function (req, res) {
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/main.js', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/RSK.jpg', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'RSK.jpg'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+    console.log(`IMAD course app listening on port ${port}!`);
 });
