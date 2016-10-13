a9ff2c5dde019a92ea04a211fcd5b0f30e0777ae
livesinmanaloor <livesinmanaloor@gmail.com>
2016-10-13 15:04:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,62 @@
-console.log('Loaded!');
+// Counter Code
+var button = document.getElementById("counter");
+
+button.onclick = function ()
+{
+  // Create a request object
+  var request = new XMLHttpRequest();
+  // Capture the response and store it in a variable
+  request.onreadystatechange = function()
+  {
+    if(request.readyState === XMLHttpRequest.DONE)
+    {
+      // Take some action
+      if(request.status === 200)
+      {
+        var counter = request.responseText;
+        // Render the variable in the correct span
+        var span = document.getElementById("count");
+        span.innerHTML = counter.toString(); 
+      }
+    }
+  // Don't do anything
+  };
+  // Make the request
+  request.open('GET','http://localhost:8080/counter',true);
+  request.send(null);
+};
+
+//Submit name
+var submit = document.getElementById("submit_btn");
+submit.onclick = function()
+{
+  // Create a request object
+  var request = new XMLHttpRequest();
+  // Capture the response and store it in a variable
+  request.onreadystatechange = function()
+  {
+    if(request.readyState === XMLHttpRequest.DONE)
+    {
+      // Take some action
+      if(request.status === 200)
+      {
+        // Capture a list of names and render it as a list
+        var names = request.responseText;
+        names = JSON.parse(names);
+        var list = '';
+        for(var i=0;i<names.length;i++)
+        {
+          list += '<li>' + names[i] + '</li>';
+        }
+        var ul = document.getElementById("nameList");
+        ul.innerHTML = list;
+      }
+    }
+    // Don't do anything
+  };
+  var nameInput = document.getElementById("name");
+  var name = nameInput.value;
+  // Make the request
+  request.open('GET','http://localhost:8080/submit-name?name=' + name,true);
+  request.send(null);
+};
